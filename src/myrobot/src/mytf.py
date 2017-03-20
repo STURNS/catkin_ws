0bc6372d42efab43ea4a7e917489a6a685b65e32
STURNS <linyuyu27906@163.com>
2017-03-20 06:47:18 -0700

    fix-tf

--- src/myrobot/src/mytf.py
+++ src/myrobot/src/mytf.py
@@ -3,27 +3,36 @@
 import roslib
 import rospy
 import tf
-from math import pi,pow
+from math import pi,pow,sin,cos
 
 class TF_trans:
+    #---------------------------------------------------------------------#
+    # initialized
+    #---------------------------------------------------------------------#	
     def __init__(self):
         rospy.init_node('tf_node')
         self.br = tf.TransformBroadcaster()
         rospy.on_shutdown(self.shutdown)
         self.r = rospy.Rate(2)
+    #---------------------------------------------------------------------#
+    # publish tf data
+    #---------------------------------------------------------------------#	
     def pub_tranforms(self):
-        rospy.loginfo('in vm')
         start_time = rospy.Time.from_sec(rospy.get_time()).to_sec()
         while not rospy.is_shutdown():
+            dt = rospy.Time.from_sec(rospy.get_time() - start_time).to_sec()
+            self.br.sendTransform((1, 0, 0),
+                tf.transformations.quaternion_from_euler(0, 0, 0),
+                rospy.Time.now(),'odom','world')
+
             self.br.sendTransform((0.1, 0, 0),
                 tf.transformations.quaternion_from_euler(0, 0, 0),
                 rospy.Time.now(),'map','odom')
-            self.br.sendTransform((0.1, 0, 0),
-                tf.transformations.quaternion_from_euler(0, 0, 0),
+
+            self.br.sendTransform((0.5*cos(dt*0.1), 0.5*sin(dt*0.1), 0),
+                tf.transformations.quaternion_from_euler(0, 0, dt*0.1),
                 rospy.Time.now(),'base_link','odom')
-            self.br.sendTransform((1, 0, 0),
-                tf.transformations.quaternion_from_euler(0, 0, pi/2),
-                rospy.Time.now(),'odom','world')
+
             #self.br.sendTransform((0, 0, 0),
             #    tf.transformations.quaternion_from_euler(0, 0, 0),
             #    rospy.Time.now(),'map','odom')
@@ -31,10 +40,16 @@ class TF_trans:
                 #rospy.Time.from_sec(rospy.get_time()).to_sec()
                 #rospy.Duration.from_sec(rospy.get_time()).to_sec()
             self.r.sleep()
+    #---------------------------------------------------------------------#
+    # shutdown
+    #---------------------------------------------------------------------#	
     def shutdown(self):
         rospy.loginfo("tf exti")
-        rospy.sleep(1)    
-        
+        rospy.sleep(1)  
+
+#---------------------------------------------------------------------#
+# tf thread
+#---------------------------------------------------------------------#	       
 if __name__ == '__main__':
     mtf = TF_trans()
     mtf.pub_tranforms()
