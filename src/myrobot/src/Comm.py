8660f9d698b9fe96fb8c67c26ddd870595315d2d
STURNS <linyuyu27906@163.com>
2017-04-26 02:32:47 -0700

    modbus ok
    Signed-off-by: STURNS <linyuyu27906@163.com>

--- src/myrobot/src/Comm.py
+++ src/myrobot/src/Comm.py
@@ -24,11 +24,14 @@ class Base_Controller:
     def __init__(self):
         rospy.init_node('comm', anonymous=False)
         rospy.on_shutdown(self.shutdown)
-        self.rate  = rospy.Rate(3) #10Hz
+        self.rate  = rospy.Rate(2) #10Hz
 
         #load the params
-        self.port =  rospy.get_param("~port", "/dev/ttyUSB1")
+        self.port =  rospy.get_param("~port", "/dev/ttyUSB0")
         self.buadrate =  rospy.get_param("~buadrate", 115200)
+        self.reg_address = rospy.get_param("~reg_address",
+            {'laser_start': 0,'laser_end': 250,'Odom_start': 360,'Odom_end': 362,'TF_start': 363,'TF_end': 368})
+           
 
         self.pub = rospy.Publisher('/laser_scan',LaserScan,queue_size=10)
         #Connect to the slave
@@ -42,33 +45,51 @@ class Base_Controller:
             self.master.set_verbose(True)
             #mb_logger.info("connecting to base_controller...")
             rospy.loginfo("connecting to base_controller...")
-            print(list(self.master.execute(1, cst.READ_HOLDING_REGISTERS, 0, 100)))
+            print(list(self.master.execute(1, cst.READ_HOLDING_REGISTERS, 350, 10)))
+
         except modbus_tk.modbus.ModbusError as exc:
             rospy.logerr("%s- Code=%d", exc, exc.get_exception_code())
     #---------------------------------------------------------------------#
     #poll
     #---------------------------------------------------------------------#          
-    def pub_data(self):
+    def get_data(self):
 
-        n = 100
+        n = self.reg_address['laser_end']-1
         lrange = pi
         laser_data = LaserScan()
         laser_data.range_max = 10
         laser_data.range_min = 0.1
         laser_data.angle_max = lrange
         laser_data.angle_min = 0
-        laser_data.angle_increment = lrange/(n-1)
+        laser_data.angle_increment = lrange/n
         for i in range(n):
             laser_data.ranges.append(0)
             laser_data.intensities.append(0)
         laser_data.header.frame_id = 'base_link'
+        
+        #get the laser data from base controller
+        din = [0]*(self.reg_address['laser_end'] - self.reg_address['laser_start'])
+        section = int((self.reg_address['laser_end'] - self.reg_address['laser_start'])/100)
+        if((self.reg_address['laser_end'] - self.reg_address['laser_start'])%100 != 0):
+            section += 1
         while not rospy.is_shutdown():
-            din = list(self.master.execute(1, cst.READ_HOLDING_REGISTERS, 0, 100))
+            try:
+                for i in range(section):
+                    r_start = i*100
+                    r_end = r_start+100                  
+                    if(r_end > self.reg_address['laser_end']):
+                        r_end = self.reg_address['laser_end']
+                    din[r_start:r_end] = list(self.master.execute(1, cst.READ_HOLDING_REGISTERS,  r_start,  (r_end- r_start)))
+            except Exception as e:
+                rospy.loginfo('get data error %s'%e)
+                pass
+            #print(din)
+            #pubulish laser data       
             laser_data.header.stamp = rospy.Time()
             for i in range(n):
                 laser_data.ranges[i] = din[i]/100
                 laser_data.intensities[i] = 0
-            self.pub.publish(laser_data)
+            self.pub.publish(laser_data)          
             self.rate.sleep()
     #---------------------------------------------------------------------#
     #poll
@@ -85,4 +106,4 @@ class Base_Controller:
 
 if __name__ == "__main__":
     BC = Base_Controller()
-    BC.pub_data()
+    BC.get_data()
